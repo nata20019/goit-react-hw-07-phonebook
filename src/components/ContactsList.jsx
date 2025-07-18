9a11e9f8ced7ff7c4822eb4f5967009ccc71559a
nata20019 <150694271+nata20019@users.noreply.github.com>
2025-07-18 15:53:44 +0200

    mod7234567

--- src/components/ContactsList.jsx
+++ src/components/ContactsList.jsx
@@ -37,35 +37,3 @@ export const ContactsList = () => {
 };
 
 export default ContactsList;
-
-// import { ContactsItem } from './ContactsItem';
-// import PropTypes from 'prop-types';
-
-// export const ContactsList = ({ contacts, onDelete }) => {
-//   return (
-//     <div className="p-group" role="group" aria-label="Basic example">
-//       <ul className="w-list">
-//         {contacts.map(contact => (
-//           <ContactsItem
-//             key={contact.id}
-//             contact={contact}
-//             onDelete={onDelete}
-//           />
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// };
-
-// ContactsList.propTypes = {
-//   contacts: PropTypes.arrayOf(
-//     PropTypes.shape({
-//       id: PropTypes.string.isRequired,
-//       name: PropTypes.string.isRequired,
-//       number: PropTypes.string.isRequired,
-//     })
-//   ).isRequired,
-//   onDelete: PropTypes.func.isRequired,
-// };
-
-// export default ContactsList;
--- src/components/ContactsSearch.jsx
+++ src/components/ContactsSearch.jsx
@@ -30,32 +30,3 @@ const ContactsSearch = () => {
 };
 
 export default ContactsSearch;
-
-// import PropTypes from 'prop-types';
-// import { nanoid } from 'nanoid';
-
-// const ContactsSearch = ({ filter, onFilterChange }) => {
-//   const searchInputId = nanoid();
-
-//   return (
-//     <div className="input-group mb-3">
-//       <label htmlFor={searchInputId} className="name-to-finde">
-//         Find contacts by name
-//       </label>
-//       <input
-//         type="text"
-//         className="form-control"
-//         placeholder="Search contact"
-//         id={searchInputId}
-//         value={filter}
-//         onChange={onFilterChange}
-//       />
-//     </div>
-//   );
-// };
-// ContactsSearch.propTypes = {
-//   filter: PropTypes.string.isRequired,
-//   onFilterChange: PropTypes.func.isRequired,
-// };
-
-// export default ContactsSearch;
